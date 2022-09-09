8f815a16d4ccb6fac02d438d7411431458c4378e
EliIvey <98063858+EliIvey@users.noreply.github.com>
2022-09-09 11:54:51 -0400

    Update git-lab-program.cc

--- git-lab-program.cc
+++ git-lab-program.cc
@@ -1,7 +1,7 @@
 /*
  *        File: git-lab-program.cc
  *      Author: Eli Ivey
- *        Date: January 19 2022
+ *        Date: JSeptember 9 2022
  * Description: Lab2
  */
 
@@ -16,3 +16,5 @@ int main(int argc, char const *argv[]) {
     cout << "Hello Git!!" << endl;
     return 0;
 }// main
+
+//I did this lab last semester so i already have the file in this repo
